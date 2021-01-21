ada5a8b3fc79071228b6c561852fd818d0238825
Dennis <lowmans00@gmail.com>
2021-01-21 18:17:12 +0900

    Update global decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,11 @@
-
+import React from 'react'
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' }
 }
+export const decorators = [
+  Story => (
+    <div style={{ margin: '2em' }}>
+      <Story />
+    </div>
+  )
+]
