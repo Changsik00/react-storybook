93fc5768d9d93d652649edc5d17f3af35e4317f6
Changsik00 <lowmans00@gmail.com>
2021-01-27 22:21:16 +0900

    Add addon ally

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app'
   ],
